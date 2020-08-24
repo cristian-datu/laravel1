1dd52430e3df38924b151eb30ba3c7c0d03a767b
Cristian Datu <datu.cristian@gmail.com>
2020-08-24 14:47:08 +0300

    Added seeding for User model. Generates 10 users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,9 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
+        'terms_accepted_at' => now(),
+        'phone' => $faker->unique()->e164PhoneNumber,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'remember_token' => null, //Str::random(10),
     ];
 });
