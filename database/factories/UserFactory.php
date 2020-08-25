2d5f627ae00422f7e5828e23f336bd75582875a9
Cristian Datu <datu.cristian@gmail.com>
2020-08-25 15:32:46 +0300

    Reintroduced remember_token generation for seeding

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,6 @@ $factory->define(User::class, function (Faker $faker) {
         'terms_accepted_at' => now(),
         'phone' => $faker->unique()->e164PhoneNumber,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => null, //Str::random(10),
+        'remember_token' => Str::random(60),
     ];
 });
