a2d4b5b996b595ee4f577334ce208a2346faad6b
Cristian Datu <datu.cristian@gmail.com>
2020-08-27 14:27:53 +0300

    Added SearchBar component

---
+++ resources/js/components/ui/SearchBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <form action="#" v-on:submit.prevent="submitSearch" class="mb-4">
+        <div class="input-group input-group-sm mb-3">
+            <input
+                v-model.trim="search"
+                v-bind:placeholder="labelText"
+                v-bind:aria-label="labelText"
+                type="text"
+                class="form-control"
+                aria-describedby="basic-addon2"
+            />
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="submit">
+                    {{ buttonText }}
+                </button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    name: "SearchBar",
+    props: {
+        labelText: { type: String, required: false, default: "Search terms" },
+        buttonText: { type: String, required: false, default: "Search" },
+        eventType: { type: String, required: false, default: "on-search" }
+    },
+    data() {
+        return {
+            search: ""
+        };
+    },
+    methods: {
+        submitSearch() {
+            this.$root.$emit(this.eventType, this.search);
+        }
+    }
+};
+</script>
+
+<style scoped></style>
