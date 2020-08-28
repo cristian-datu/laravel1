1cb5c3d123d3cc4d928c161a3f6b1570fe02b4e1
Cristian Datu <datu.cristian@gmail.com>
2020-08-28 20:58:49 +0300

    Setup fillabel and casts for TermOfservice

--- app/TermOfService.php
+++ app/TermOfService.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class TermOfService extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'content', 'published_at'];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
 }
